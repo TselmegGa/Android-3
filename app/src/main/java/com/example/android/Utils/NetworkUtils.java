f92d170ed6321fea362a2923e5adecfc1d046e4c
Tselmeg <g.tselmeg@yahoo.com>
2020-02-11 12:11:11 +0100

    Set up
    Set up

---
+++ app/src/main/java/com/example/android/Utils/NetworkUtils.java
@@ -0,0 +1,56 @@
+package com.example.android.Utils;
+
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public final class NetworkUtils {
+    public static final String TAG = NetworkUtils.class.getSimpleName();
+    public static final String URL = "https://services7.arcgis.com/21GdwfcLrnTpiju8/arcgis/rest/services/Sierende_elementen/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json";
+
+    public static URL buildUrl() {
+        URL url = null;
+        try {
+            url = new URL(URL);
+        } catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+
+        Log.v(TAG, "Built URL " + url);
+        return url;
+    }
+    public static String sendGET(URL url, String token) throws IOException {
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        StringBuffer response = new StringBuffer();
+        con.setRequestMethod("GET");
+        int responseCode = con.getResponseCode();
+        System.out.println("GET Response Code :: " + responseCode);
+        if (responseCode == HttpURLConnection.HTTP_OK) { // success
+            BufferedReader in = new BufferedReader(new InputStreamReader(
+                    con.getInputStream()));
+            String inputLine;
+
+            System.out.println(response.toString());
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            // print result
+
+        } else {
+            return "GET request not worked";
+        }
+        Log.v(TAG, "GET " + url);
+        return response.toString();
+    }
+
+
+
+}
