265fa8310fbca143fcd6d3e1fa9d9091d6b3158f
Tselmeg <g.tselmeg@yahoo.com>
2020-02-12 19:33:03 +0100

    filter
    filter

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -22,11 +27,20 @@ public class MainActivity extends AppCompatActivity implements MainOnclickHandle
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+    private SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view);
+        sharedPreferences = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        SharedPreferences.Editor edit = sharedPreferences.edit();
+        edit.putBoolean("stone", true);
+        edit.putBoolean("metal", true);
+        edit.putBoolean("glass", true);
+        edit.putBoolean("bronze", true);
+        edit.putBoolean("iron", true);
+        edit.apply();
 
         //obtain a handle to the object
         mRecyclerView = findViewById(R.id.recycler_view_activity_list);
@@ -36,20 +50,56 @@ public class MainActivity extends AppCompatActivity implements MainOnclickHandle
         mRecyclerView.setLayoutManager(mLayoutManager);
 
         //Init features array
-        ArrayList<Feature> mFeatures = new ArrayList<>();
-
-        //Get features
-        try {
-            mFeatures = new GetItemAsyncTask().execute().get();
-        } catch (ExecutionException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        ArrayList<Feature> mFeatures = getFeatures();
 
         Toast.makeText(this, "Aantal opgehaalde items: " + mFeatures.size(), Toast.LENGTH_LONG).show();
 
         // specify an adapter
+        setAdapter(mFeatures);
+    }
+    private void setAdapter(ArrayList<Feature> mFeatures){
         mAdapter = new MainRecycleViewAdapter(mFeatures, this);
         mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private ArrayList<Feature> getFeatures(){
+        ArrayList<Feature> features = new ArrayList<>();
+
+        try {
+            features = new GetItemAsyncTask().execute().get();
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        return features;
+    }
+
+    private ArrayList<Feature> getPrefFeatures(ArrayList<Feature> features){
+        String material = null;
+        ArrayList<Feature> prefFeatures = new ArrayList<>();
+        for (Feature item: features) {
+            Log.v("",item.getMaterial());
+            if (item.getMaterial().contains("Bronze")|| item.getMaterial().contains("bronze") || item.getMaterial().contains("Brons")|| item.getMaterial().contains("brons")){
+                material = "bronze";
+            }
+            else if (item.getMaterial().contains("Steen")|| item.getMaterial().contains("steen")){
+                material = "stone";
+            }
+            else if (item.getMaterial().contains("Glas")|| item.getMaterial().contains("glas")){
+                material = "glass";
+            }
+            else if (item.getMaterial().contains("Metaal")|| item.getMaterial().contains("metaal")){
+                material = "metal";
+            }
+            else if (item.getMaterial().contains("Ijzer")|| item.getMaterial().contains("ijzer")){
+                material = "iron";
+            }
+            if(sharedPreferences.getBoolean(material,false)){
+                prefFeatures.add(item);
+            }
+
+        }
+
+        return prefFeatures;
     }
 
     @Override
@@ -66,5 +116,77 @@ public class MainActivity extends AppCompatActivity implements MainOnclickHandle
 
         Log.d(this.getClass().getSimpleName(), "Starting intent: " + FeatureDetails.class.getSimpleName());
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_material, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        SharedPreferences.Editor edit = sharedPreferences.edit();
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.material_stone:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                    edit.putBoolean("stone", false);
+                }
+                else {
+                    item.setChecked(true);
+                    edit.putBoolean("stone", true);
+                }
+                edit.apply();
+                break;
+            case R.id.material_metal:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                    edit.putBoolean("metal", false);
+                }
+                else {
+                    item.setChecked(true);
+                    edit.putBoolean("metal", true);
+                }
+                edit.apply();
+                break;
+            case R.id.material_glass:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                    edit.putBoolean("glass", false);
+                }
+                else {
+                    item.setChecked(true);
+                    edit.putBoolean("glass", true);
+                }
+                edit.apply();
+                break;
+            case R.id.material_bronze:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                    edit.putBoolean("bronze", false);
+                }
+                else {
+                    item.setChecked(true);
+                    edit.putBoolean("bronze", true);
+                }
+                edit.apply();
+                break;
+            case R.id.material_iron:
+                if(item.isChecked()){
+                    item.setChecked(false);
+                    edit.putBoolean("iron", false);
+                }
+                else {
+                    item.setChecked(true);
+                    edit.putBoolean("iron", true);
+                }
+                edit.apply();
+                break;
+        }
+        setAdapter(getPrefFeatures(getFeatures()));
+        return true;
     }
 }
